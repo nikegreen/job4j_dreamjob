7f1534df9f5fb1297138de8e06643bd6e49b2f5e
nikez <nikegreen@yandex.ru>
2022-12-16 22:43:38 +0500

    1. Что такое клиент - серверное приложение. [#504837] - 2 минимальное приложение spring boot

--- src/main/java/ru/job4j/dreamjob/Main.java
+++ src/main/java/ru/job4j/dreamjob/Main.java
@@ -3,8 +3,19 @@ package ru.job4j.dreamjob;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+/**
+ * <p>Main class.</p>
+ *
+ * @author nikez
+ * @version $Id: $Id
+ */
 @SpringBootApplication
 public class Main {
+    /**
+     * <p>main.</p>
+     *
+     * @param args an array of {@link java.lang.String} objects.
+     */
     public static void main(String[] args) {
         SpringApplication.run(Main.class, args);
     }
--- src/main/java/ru/job4j/dreamjob/controller/IndexControl.java
+++ src/main/java/ru/job4j/dreamjob/controller/IndexControl.java
@@ -3,9 +3,20 @@ package ru.job4j.dreamjob.controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+/**
+ * <p>IndexControl class.</p>
+ *
+ * @author nikez
+ * @version $Id: $Id
+ */
 @RestController
 public class IndexControl {
 
+    /**
+     * <p>index.</p>
+     *
+     * @return a {@link java.lang.String} object.
+     */
     @GetMapping("/index")
     public String index() {
         return "Greetings from Spring Boot!";
