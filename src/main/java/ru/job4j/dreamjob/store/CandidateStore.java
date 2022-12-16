f3d25313507cc879d854e7f83438d8678f2e344e
nikez <nikegreen@yandex.ru>
2022-12-17 00:33:13 +0500

    5. Список кандидатов [#504842]

---
+++ src/main/java/ru/job4j/dreamjob/store/CandidateStore.java
@@ -0,0 +1,28 @@
+package ru.job4j.dreamjob.store;
+
+import ru.job4j.dreamjob.model.Candidate;
+import ru.job4j.dreamjob.model.Post;
+
+import java.util.Collection;
+import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
+
+public class CandidateStore {
+    private static final CandidateStore INST = new CandidateStore();
+
+    private final Map<Integer, Candidate> candidates = new ConcurrentHashMap<>();
+
+    private CandidateStore() {
+        candidates.put(1, new Candidate(1, "Bob Sinkler", "middle", "10.12.2022"));
+        candidates.put(2, new Candidate(2, "Bill Gates", "senior", "1.11.2019"));
+        candidates.put(3, new Candidate(3, "Erik Krause", "junior", "16.01.2001"));
+    }
+
+    public static CandidateStore instOf() {
+        return INST;
+    }
+
+    public Collection<Candidate> findAll() {
+        return candidates.values();
+    }
+}
